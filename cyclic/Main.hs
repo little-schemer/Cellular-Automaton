908e2990727ac71df67c12dcf7223bfa19c1db6f
Shozo Shinmura <the.little.schemer@gmail.com>
2019-04-03 16:55:35 +0900

    modify cyclic/Main.hs

--- cyclic/Main.hs
+++ cyclic/Main.hs
@@ -22,35 +22,29 @@ colors = [makeColorI r g 255 a | (r, g, a) <- zip3 rLst gLst aLst]
 
 state :: Int
 state = 10
-
 main :: IO ()
 main = do
   cells <- replicateM (width field * height field) (randomRIO (0, state))
-  simulate window black 15 (Vec.fromList cells) (drawCells field) (simCells field)
+  simulate window black 15 (Vec.fromList cells) (draw field) (simCells field)
     where
       field = Field { width = 200, height = 150, cellSize = 5 }
       sizeX = width field * (round (cellSize field))
       sizeY = height field * (round (cellSize field))
       window = InWindow "Cyclic" (sizeX, sizeY) (10, 10)
 
-drawCells :: Field -> Cells -> Picture
-drawCells field cells = Pictures $ Vec.toList $ Vec.imap f cells
+draw :: Field -> Cells -> Picture
+draw field cells = Pictures $ Vec.toList $ Vec.imap drawCell cells
   where
-    f i cell = Translate x y $ Color (colors !! cell) $ rectangleSolid size size
+    drawCell i cell =
+      Translate x y $ Color (colors !! cell) $ rectangleSolid size size
       where
         (x, y) = indexToGlossPoint field i
         size = cellSize field - 1
 
 simCells :: Field -> ViewPort -> Float -> Cells -> Cells
-simCells field _ time cells = Vec.imap check cells
+simCells field _ _ cells = Vec.imap check cells
   where
     check i c = if (elem (mod (c + 1) state) lst) then (mod (c + 1) state) else c
       where
-        lst = map g [(x, py), (mx,  y), (px, y), (x, my)]
-          where
-            (x, y) = indexToPos field i
-            mx = mod (x - 1) (width field)
-            px = mod (x + 1) (width field)
-            my = mod (y - 1) (height field)
-            py = mod (y + 1) (height field)
-            g (x', y') = cells Vec.! (posToIndex field (x', y'))
+        f (x, y) = cells Vec.! (posToIndex field (x, y))
+        lst = [f (x, y) | (x, y) <- vonNeumannN field (indexToPos field i)]
