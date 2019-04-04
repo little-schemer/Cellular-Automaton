d45fd8345c98da4641f336ce767ad1f630264a26
Shozo Shinmura <the.little.schemer@gmail.com>
2019-04-04 15:14:06 +0900

    modify life-game/Main.hs
    life-game/Main.hs
      + 試しにステップ数を表示するように改変。

--- life-game/Main.hs
+++ life-game/Main.hs
@@ -9,14 +9,15 @@ import Field
 
 
 type Vec   = Vec.Vector
-type Cells = Vec Bool
+data Cells = Cells { cells :: Vec Bool, count :: Int }
 
 
 main :: IO ()
 main = do
-  cells <- replicateM (width field * height field) randomIO
+  bs <- replicateM (width field * height field) randomIO
   -- let cells = setCell field [(10,10), (11,10),(12,10),(10,11),(11,12)] -- グライダー
-  simulate window black 10 (Vec.fromList cells) (draw field) (simCells field)
+  let cellData = Cells { cells = Vec.fromList bs, count = 0 }
+  simulate window black 10 cellData (draw field) (simCells field)
     where
       field = Field { width = 200, height = 150, cellSize = 5}
       sizeX = width field * (truncate (cellSize field))
@@ -24,7 +25,7 @@ main = do
       window = InWindow "Life Game" (sizeX, sizeY) (10, 10)
 
 draw :: Field -> Cells -> Picture
-draw field cells = Pictures $ Vec.toList $ Vec.imap drawCell cells
+draw field cs = Pictures $ ((Vec.toList $ Vec.imap drawCell (cells cs))) ++ [Color red $ Text (show (count cs))]
   where
     drawCell i cell = if cell
                       then Translate x y $ Color cyan $ rectangleSolid size size
@@ -34,22 +35,22 @@ draw field cells = Pictures $ Vec.toList $ Vec.imap drawCell cells
         size = cellSize field - 1
 
 simCells :: Field -> ViewPort -> Float -> Cells -> Cells
-simCells field _ _ cells = Vec.imap check cells
+simCells field _ _ cs = cs { cells = Vec.imap check (cells cs), count = count cs + 1 }
   where
     check i cell
       | cell      = if (count == 2 || count == 3) then True else False
       | otherwise = if (count == 3)               then True else False
       where
-        f (x, y) = cells Vec.! (posToIndex field (x, y))
+        f (x, y) = (cells cs) Vec.! (posToIndex field (x, y))
         bs = [f (x, y) | (x, y) <- mooreN field (indexToPos field i)]
         count = length $ filter (\x -> x) bs
 
-setDataAt :: [a] -> [(Int, a)] -> [a]
-setDataAt xs as = foldl f xs as
-  where f xs (i, d) = let (as, _ : bs) = splitAt i xs in as ++ (d : bs)
+-- setDataAt :: [a] -> [(Int, a)] -> [a]
+-- setDataAt xs as = foldl f xs as
+--   where f xs (i, d) = let (as, _ : bs) = splitAt i xs in as ++ (d : bs)
 
-setCell :: Field -> [Position] -> [Bool]
-setCell field xs = setDataAt cells $ map f xs
-  where
-    cells = replicate (width field * height field) False
-    f (x, y) = (posToIndex field (x, y), True)
+-- setCell :: Field -> [Position] -> [Bool]
+-- setCell field xs = setDataAt cells $ map f xs
+--   where
+--     cells = replicate (width field * height field) False
+--     f (x, y) = (posToIndex field (x, y), True)
