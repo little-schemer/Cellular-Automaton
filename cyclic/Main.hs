5fd7c60dcb9cb0ea86f1c649ecef30bc668576fd
Shozo Shinmura <the.little.schemer@gmail.com>
2019-04-18 16:08:35 +0900

    modify cyclic/Main.hs
    + cyclic/Main.hs
      - コマンドラインオプションを追加。

--- cyclic/Main.hs
+++ cyclic/Main.hs
@@ -4,6 +4,7 @@ module Main where
 import qualified Data.Vector as Vec
 import System.Random
 import Control.Monad
+import System.Environment
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Field
@@ -21,27 +22,40 @@ cells  =   5 :: Float
 
 main :: IO ()
 main = do
-  cells <- replicateM (width * height) (randomRIO (0, state))
-  simulate window black 15 (Vec.fromList cells) (draw field) (simCells field)
+  args <- getArgs
+  let (st, nb) = cmd args
+  cells <- replicateM (width * height) (randomRIO (0, st))
+  simulate window black 20
+    (Vec.fromList cells) (draw field (colors st)) (simCells field st nb)
     where
       field = initField width height cells
       window = InWindow "Cyclic" (windowSize field) (0, 0)
 
-colors :: [Color]
-colors = [makeColorI r g 150 a | (r, g, a) <- zip3 rLst gLst aLst]
+cmd :: [String] -> (Int, Field -> Position -> [Position])
+cmd args = loop args (state, vonNeumannN)
   where
-    rLst = [0, (div 255 state) .. 255]
-    gLst = [255, 255 - (div 255 state) .. 0]
-    aLst = cycle [150, 200]
+    loop [] opt = opt
+    loop ("-s" : s : args') (_ , nb) = loop args' (read s, nb)
+    loop ("-n" : "n" : args') (st, _) = loop args' (st, vonNeumannN)
+    loop ("-n" : "m" : args') (st, _) = loop args' (st, mooreN)
+    loop (_ : _ : args') (st, nb) = loop args' (st, nb)
 
-draw :: Field -> Cells -> Picture
-draw field cells = Pictures $ Vec.toList $ Vec.imap drawCell cells
-  where drawCell i cell = Color (colors !! cell) $ indexToDrawCell field i
-
-simCells :: Field -> ViewPort -> Float -> Cells -> Cells
-simCells field _ _ cells = Vec.imap check cells
+colors :: Int -> [Color]
+colors st = [makeColorI r g 150 a | (r, g, a) <- zip3 rLst gLst aLst]
   where
-    check i c = if (elem (mod (c + 1) state) lst) then (mod (c + 1) state) else c
+    rLst = [0, (div 255 st) .. 255]
+    gLst = [255, 255 - (div 255 st) .. 0]
+    aLst = cycle [100, 200, 150, 250]
+
+draw :: Field -> [Color] -> Cells -> Picture
+draw field clrs cells = Pictures $ Vec.toList $ Vec.imap drawCell cells
+  where drawCell i cell = Color (clrs !! cell) $ indexToDrawCell field i
+
+simCells :: Field -> Int -> (Field -> Position -> [Position])
+         -> ViewPort -> Float -> Cells -> Cells
+simCells field st nb _ _ cells = Vec.imap check cells
+  where
+    check i c = if (elem (mod (c + 1) st) lst) then (mod (c + 1) st) else c
       where
         f (x, y) = cells Vec.! (posToIndex field (x, y))
-        lst = [f (x, y) | (x, y) <- vonNeumannN field (indexToPos field i)]
+        lst = [f (x, y) | (x, y) <- nb field (indexToPos field i)]
