082de445bbdda85e0502f47bb1605d4ee5270651
Shozo Shinmura <the.little.schemer@gmail.com>
2020-01-12 12:41:02 +0900

    Update : langton/Main.hs

--- langton/Main.hs
+++ langton/Main.hs
@@ -4,18 +4,22 @@ import           Control.Monad
 import           Data.Maybe
 import qualified Data.Vector                  as V
 import qualified Data.Vector.Mutable          as M
+import qualified Data.Vector.Unboxed          as VU
 import           Field
 import           Graphics.Gloss
 import           Graphics.Gloss.Data.ViewPort
 import           System.Random
 
 
+type State = Int
+type Motion = Int
+
 data Model = Model { cells   :: VU.Vector Color
                    , antHead :: Int
                    , antPos  :: Position
                    , antSt   :: Int
                    , rule    :: [((Color, State), (Color, Motion, State))]
-                   } deriving Show
+                   } -- deriving Show
 
 width  = 300 :: Int
 height = 200 :: Int
@@ -47,18 +51,18 @@ main = simulate window black 15 model drawModel simAnt
 drawModel :: Model -> Picture
 drawModel md = Pictures [cellPic, ant]
   where
-    cellPic = Pictures $ V.toList $ V.imap drawCell (cells md)
-    ant = Color cyan $ posToDrawCell fd (antPos md)
+    cellPic = Pictures $ V.toList $ V.imap drawCell $ VU.convert (cells md)
+    ant = indexToDrawCell field (posToIndex window $ antPos model) cyan
     drawCell i cell = if (cell /= black)
-                      then Color cell $ indexToDrawCell fd i
+                      then indexToDrawCell fd i cell
                       else Blank
 
-simAnt :: Field -> ViewPort -> Float -> Model -> Model
-simAnt fd _ _ md = md { cells = cells', antHead = antHead', antPos = antPos' }
+simAnt :: ViewPort -> Float -> Model -> Model
+simAnt _ _ md = md { cells = cells', antHead = antHead', antPos = antPos' }
   where
-    antIdx = posToIndex fd $ antPos md
+    antIdx = posToIndex field $ antPos md
     (color, move, state) = fromJust $ lookup (cellColor, antSt md) (rule md)
       where cellColor = (cells md) V.! antIdx
-    cells' = V.modify (\v -> M.write v antIdx color) (cells md)
+    cells' = VU.modify (\v -> M.write v antIdx color) (cells md)
     antHead' = mod (antHead md + move) 4
     antPos' = (vonNeumannN fd $ antPos md) !! antHead'
