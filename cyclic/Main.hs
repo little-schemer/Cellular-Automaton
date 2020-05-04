3511dabfacf79d4813d2d9bf64d14a8dfe46be92
Shozo Shinmura <the.little.schemer@gmail.com>
2020-05-04 22:19:21 +0900

    Update : cyclic/Main.hs にコマンドオプションを追加。

--- cyclic/Main.hs
+++ cyclic/Main.hs
@@ -15,36 +15,57 @@ import qualified Data.Vector.Unboxed          as VU
 import           Field
 import           Graphics.Gloss
 import           Graphics.Gloss.Data.ViewPort
-import           System.Environment
+import           Options.Applicative
 import           System.Random
 
 
 type Model = (VU.Vector Int, Int, [Color])
 
 
+---------------------------------------------------
+-- * パラメータ
+---------------------------------------------------
+
 width  = 300 :: Int
 height = 200 :: Int
-size   =   4 :: Float
+size   =   3 :: Float
 state  =  16 :: Int
-field  = initField width height size neumann
 
+
+---------------------------------------------------
+-- * コマンドオプション
+---------------------------------------------------
+
+data Option = Option
+  { fdWidth  :: Int
+  , fdHeight :: Int
+  , neighbor :: String
+  , cState   :: Int
+  }
+
+opt :: Parser Option
+opt = Option
+  <$> option auto (short 'w' <> value width)
+  <*> option auto (short 'h' <> value height)
+  <*> strOption   (short 'n' <> value "n")
+  <*> option auto (short 's' <> value state)
+
+
+---------------------------------------------------
+-- * Cyclic
+---------------------------------------------------
 
 main :: IO ()
 main = do
-  cells <- VU.replicateM (width * height) (randomRIO (0, state))
-  simulate window black 15 (cells, state, colors state) drawModel simModel
-    where window = InWindow "Cyclic" (windowSize width height size) (0, 0)
-
-
--- cmd :: [String] -> (Int, Int)
--- cmd args = loop args (state, fst)
---   where
---     loop [] opt                       = opt
---     loop ("-s" : s : args') (_, nb)   = loop args' (read s, nb)
---     loop ("-n" : "n" : args') (st, _) = loop args' (st, fst)
---     loop ("-n" : "m" : args') (st, _) = loop args' (st, snd)
---     loop (_ : args') (st, nb)         = loop args' (st, nb)
-
+  Option w h n st <- execParser (info opt mempty)
+  let fd = initField w h size (nf n)
+  cells <- VU.replicateM (w * h) (randomRIO (0, st))
+  let model = (cells, st, colors st)
+  let window = InWindow "Cyclic" (windowSize fd) (0, 0)
+  simulate window black 15 model (drawModel fd) (simModel fd)
+    where
+      nf "n" = neumann
+      nf "m" = moore
 
 colors :: Int -> [Color]
 colors st = [makeColorI r g 150 a | (r, g, a) <- zip3 rLst gLst aLst]
@@ -53,17 +74,16 @@ colors st = [makeColorI r g 150 a | (r, g, a) <- zip3 rLst gLst aLst]
     gLst = [255, 255 - (div 255 st) .. 0]
     aLst = cycle [100, 200, 150, 250]
 
+drawModel :: Field -> Model -> Picture
+drawModel fd (cells, st, clrs) = Pictures $ V.toList $ V.imap drawCell $ V.convert cells
+  where drawCell i cell = indexToDrawCell fd i (clrs !! cell)
 
-drawModel :: Model -> Picture
-drawModel (cells, st, clrs) = Pictures $ V.toList $ V.imap drawCell $ V.convert cells
-  where drawCell i cell = indexToDrawCell field i (clrs !! cell)
 
-
-simModel :: ViewPort -> Float -> Model -> Model
-simModel _ _ (cells, st, clrs) = (VU.imap check cells, st, clrs)
+simModel :: Field -> ViewPort -> Float -> Model -> Model
+simModel fd _ _ (cells, st, clrs) = (VU.imap check cells, st, clrs)
   where
     check i c = if elem c' clrList then c' else c
       where
         c' = mod (c + 1) st
-        posList = (neighborhoodTable field) V.! i
+        posList = (neighborTbl fd) V.! i
         clrList = map (cells VU.!) posList
